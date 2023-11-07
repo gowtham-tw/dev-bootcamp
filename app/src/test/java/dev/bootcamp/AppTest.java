7ca31bf65e15040c871a3ba3fc4b8edbe1ae563c
karthickvellingiritw <karthick.v@thoughtworks.com>
2023-11-07 09:39:56 +0530

    [Karthick] Test, Ignore it

--- app/src/test/java/dev/bootcamp/AppTest.java
+++ app/src/test/java/dev/bootcamp/AppTest.java
@@ -10,5 +10,7 @@ class AppTest {
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+
+
     }
 }
